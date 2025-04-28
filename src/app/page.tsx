250c3455d96dafee7d108c8c756ac046aaff6e94
Edward Bocaranda <edwarddevp@gmail.com>
2025-04-28 18:41:53 -0400

    Implement Clerk authentication with SignIn and SignUp pages, update package dependencies, and add middleware for protected routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import {
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+} from "@clerk/nextjs";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      HomePage
+      <br />
+      <br />
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+    </div>
+  );
 };
 
 export default HomePage;
