827dbcc28c5a85a8172e13705ba6e672c45c6c5b
Edward Bocaranda <edwarddevp@gmail.com>
2025-04-29 15:29:52 -0400

    feat: integrate Convex for backend functionality

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://singular-marten-58.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
